177c29897edf1b083c7501948a837e02277816e4
moeenn <moeen.v8@gmail.com>
2022-06-01 18:27:29 +0500

    updated jest config and remove unnecessary packages

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,6 @@
-import type { Config } from "@jest/types"
-
-const config: Config.InitialOptions = {
+export default {
   preset: "ts-jest",
   testEnvironment: "node",
   verbose: true,
   modulePathIgnorePatterns: ["<rootDir>/build/"],
 }
-
-export default config
