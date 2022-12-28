a18bce68ad95434a43f73a6246244a85dbcbbcfa
moeenn <moeen.v8@gmail.com>
2022-12-28 17:53:30 +0500

    simplified test structure

--- src/utilities/index.test.ts
+++ src/utilities/index.test.ts
@@ -1,12 +1,10 @@
-import { describe, it, expect } from "vitest"
+import { test, expect } from "vitest"
 import { add } from "@/utilities"
 
-describe("check math functions", () => {
-  it("add function", () => {
-    const input = [10, 20]
-    const expected = 30
-    const got = add(input[0], input[1])
+test("add function", () => {
+  const input = [10, 20]
+  const expected = 30
+  const got = add(input[0], input[1])
 
-    expect(got).toBe(expected)
-  })
+  expect(got).toBe(expected)
 })
