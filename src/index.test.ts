3e93e580fd533042de66453c88a2b6db04800d1a
moeenn <moeen.v8@gmail.com>
2022-12-28 16:04:02 +0500

    simplified test structure

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,9 @@
-import { describe, it, expect } from "vitest"
+import { test, expect } from "vitest"
 import { main } from "."
 
-describe("hello message", () => {
-  it("returns hello message", () => {
-    const expected = "Hello world"
-    const got = main()
-    
-    expect(got).toBe(expected)
-  })
+test("returns hello message", () => {
+  const expected = "Hello world"
+  const got = main()
+
+  expect(got).toBe(expected)
 })
