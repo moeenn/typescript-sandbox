c163db5df171dd73873546b2ef0707f53a6a807d
moeenn <moeen.v8@gmail.com>
2023-05-04 14:21:43 +0500

    code cleanup

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "module-alias/register"
 
 import { Entity } from "@/entity/entity"
 import { Position } from "./entity/position"
-import { Direction } from "./direction"
+import { Direction } from "@/direction"
 
 export async function main(): Promise<string> {
   const e = new Entity("Entity one", new Position(10, 20))
